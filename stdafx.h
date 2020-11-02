2083be4cd1e0562c642403d7a861ed4e5abbc550
Chase Hender <66840229+sFIsAnExpert@users.noreply.github.com>
2020-11-01 18:43:10 -1000

    Delete stdafx.h

--- stdafx.h
+++
@@ -1,6 +0,0 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
-#define DEFAULT_BUFLEN 512
-#define SERVERERROR std::cout << "Error: " << WSAGetLastError() << std::endl;
-#define DOT std::cout << ".";
-#define DOTSLEEP Sleep(600);
-#define DEFAULT_INTBUF 512
